0c3981a02bf29309b892a3019b820b9b9150ec5b
Olasunkanmi <olasunkanmiibalogun@gmail.com>
2022-06-15 11:07:38 +0100

    more docs

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 // What I understand by this file?
-// The _app.js file is kind of the the root file. It accepts two parameters. The Component and the pageProps. The component hold the content of the page that should be rerendered, while the pageProps represents the props of the page
+// The _app.js file is kind of the the root file. It accepts two parameters. The Component and the pageProps. The component holds the content of the page that should be rerendered, while the pageProps represents the props of the page
 
 import Layout from '../components/layout/Layout';
 import '../styles/globals.css'
